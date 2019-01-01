d2adf3dbc4841c9cde1f634bc5dbcbd56f2ea768
Chad Malfait <malfaitc@gmail.com>
2019-01-01 14:25:12 -0700

    fix default run level

--- ostack.bash
+++ ostack.bash
@@ -16,8 +16,8 @@ sudo yum install -y openstack-packstack
 yum install lvm2
 systemctl enable lvm2-lvmetad.service
 systemctl start lvm2-lvmetad.service
-pvcreate /dev/sda
-vgcreate cinder-volumes /dev/sda
+pvcreate /dev/sdb
+vgcreate cinder-volumes /dev/sdb
 
 packstack --allinone --provision-demo=n --os-neutron-ovs-bridge-mappings=extnet:br-ex --os-neutron-ovs-bridge-interfaces=br-ex:enp7s0 --os-neutron-ml2-type-drivers=vxlan,flat --cinder-backend=lvm --cinder-volumes-size=250G --cinder-volume-name=cinder-volumes --default-password='Chang3m3' --os-neutron-lbaas-install=y --os-swift-install=y
 
@@ -43,6 +43,9 @@ OVS_BRIDGE=br-ex
 ONBOOT=yes
 EOF
 
+##to free up port 6000 X wants it as well
+systemctl set-default multi-user.target
+
 reboot
 
 source /root/keystonerc_admin
@@ -58,8 +61,10 @@ openstack network create internal_network
 openstack subnet create --network internal_network --dhcp --dns-nameserver 192.168.0.1 --allocation-pool start=10.0.0.10,end=10.0.0.250 --subnet-range 10.0.0.0/8 internal_subnet
 openstack router add subnet router1 internal_subnet
 
-#curl  https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2c | glance image-create --name='centos7' --visibility=public --container-format=bare --disk-format=qcow2
+curl https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2c | glance image-create --name='centos7' --visibility=public --container-format=bare --disk-format=qcow2
 
-#curl |http://cloud.centos.org/centos/6/images/CentOS-6-x86_64-GenericCloud.qcow2c | glance image-create --name='centos6' --visibility=public --container-format=bare --disk-format=qcow2
+curl https://cloud.centos.org/centos/6/images/CentOS-6-x86_64-GenericCloud.qcow2c | glance image-create --name='centos6' --visibility=public --container-format=bare --disk-format=qcow2
 
-#curl http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img | glance image-create --name='cirros image' --visibility=public --container-format=bare --disk-format=qcow2
+curl http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img | glance image-create --name='cirros image' --visibility=public --container-format=bare --disk-format=qcow2
+
+
