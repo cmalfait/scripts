32717d353f31dbae4eaa9fa4f9eb4c582d0696cc
Chad Malfait <malfaitc@gmail.com>
2019-01-01 14:34:33 -0700

    defaults

--- ostack.bash
+++ ostack.bash
@@ -67,4 +67,9 @@ curl https://cloud.centos.org/centos/6/images/CentOS-6-x86_64-GenericCloud.qcow2
 
 curl http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img | glance image-create --name='cirros image' --visibility=public --container-format=bare --disk-format=qcow2
 
+#increase defaults
+openstack quota set --instances 20 default
+openstack quota set --secgroups 30 default
+openstack quota set --ram 60000 default
+openstack quota set --volumes 20 default
 
